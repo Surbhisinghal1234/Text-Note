cf04eec6ce216cf6758b7a9c5e311ea2d5312c8a
Surbhisinghal1234 <surbhi98singhal@gmail.com>
2024-02-06 20:42:40 +0530

    scrollbar added

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ btn.addEventListener("click", addNote);
 function addNote() {
   const text = document.getElementById("noteText").value;
   const bgColor = document.getElementById("bgColor").value;
-  const textColor =document.getElementById("textColor").value
+  const textColor = document.getElementById("textColor").value
   const textDisplay = document.getElementById("textDisplay");
   const textEle = document.createElement("div");
+  textEle.contentEditable = "true"
 
   textEle.classList.add("noteBox");
   textEle.innerHTML = text;
-  textEle.style.backgroundColor =bgColor;
-  textEle.style.color= textColor;
+  textEle.style.backgroundColor = bgColor;
+  textEle.style.color = textColor;
   const btn2 = document.createElement("a");
   btn2.classList.add("closeBtn");
   textEle.append(btn2);
