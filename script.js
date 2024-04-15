852dfa2d2348956ff376ac376a10672e9b450b6d
Surbhisinghal1234 <surbhi98singhal@gmail.com>
2024-04-15 22:56:06 +0530

    add error message

--- script.js
+++ script.js
@@ -2,28 +2,31 @@ const btn = document.querySelector("#btn");
 btn.addEventListener("click", addNote);
 
 function addNote() {
-  const text = document.getElementById("noteText").value;
+  const text = document.getElementById("noteText").value
   const bgColor = document.getElementById("bgColor").value;
   const textColor = document.getElementById("textColor").value;
   const textDisplay = document.getElementById("textDisplay");
 
-  const textEle = document.createElement("div");
-  textEle.classList.add("noteBox");
-  textEle.innerHTML = text;
-  textEle.style.backgroundColor = bgColor;
-  textEle.style.color = textColor;
-  textEle.contentEditable = "true";
-  
-  const btn2 = document.createElement("a");
-  btn2.classList.add("closeBtn");
-  btn2.innerHTML = "&times;";
-  textEle.appendChild(btn2);
-  textDisplay.appendChild(textEle);
+  if (text === "") {
+    alert("Please enter some text");
+  } else {
+    const textEle = document.createElement("div");
+    textEle.classList.add("noteBox");
+    textEle.innerHTML = text;
+    textEle.style.backgroundColor = bgColor;
+    textEle.style.color = textColor;
+    textEle.contentEditable = "true";
+    
+    const btn2 = document.createElement("a");
+    btn2.classList.add("closeBtn");
+    btn2.innerHTML = "&times;";
+    textEle.appendChild(btn2);
+    textDisplay.appendChild(textEle);
 
-  document.getElementById("noteText").value = "";
-  btn2.addEventListener("click", () => {
-    textDisplay.removeChild(textEle);
-  });
+    document.getElementById("noteText").value = "";
+
+    btn2.addEventListener("click", () => {
+      textDisplay.removeChild(textEle);
+    });
+  }
 }
-
-
